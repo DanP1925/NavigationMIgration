593070c1f3bbb1bee5128fe5e85d067fbc7ec1d7
Daniel Peñaloza <DanP1925@gmail.com>
2024-12-15 22:02:21 +0100

    feat: Add toolbar

--- app/src/main/java/com/danp1925/navigationmigration/FirstFragment.kt
+++ app/src/main/java/com/danp1925/navigationmigration/FirstFragment.kt
@@ -1,5 +1,16 @@
 package com.danp1925.navigationmigration
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 
-class FirstFragment : Fragment(R.layout.fragment_first)
+class FirstFragment : Fragment(R.layout.fragment_first){
+
+    private val mainViewModel : MainViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        mainViewModel.updateToolbar("First Screen")
+    }
+
+}
--- app/src/main/java/com/danp1925/navigationmigration/MainActivity.kt
+++ app/src/main/java/com/danp1925/navigationmigration/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.danp1925.navigationmigration
 
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +25,16 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        toolbar = findViewById(R.id.toolbar)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainViewModel.toolbarTitle.collect { toolbarTitle ->
+                    toolbar.title = toolbarTitle
+                }
+            }
         }
     }
 }
